9725f4d1051631f7fbcf51f2db9dec85b77c2070
govind sankar <gsankarvg@gmail.com>
2025-04-07 15:46:24 +0530

    commit - 9

--- q3.py
+++ q3.py
@@ -1,18 +1,18 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("Valley.png",0)
+img = cv2.imread("Road.png",0)
 
-aspect_ratio = img.shape[1] / img.shape[0]  # width/height
+aspect_ratio = img.shape[1] / img.shape[0] 
 new_height = 600
 new_width = int(new_height * aspect_ratio)
 resized_img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
 
 mean_intensity = np.mean(img)
 std_intensity = np.std(img)
-
 lower = int(max(0, mean_intensity - std_intensity))
 upper = int(min(255, mean_intensity + std_intensity))
+
 
 canny = cv2.Canny(img,lower,upper)
 resized_edge = cv2.resize(canny, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
